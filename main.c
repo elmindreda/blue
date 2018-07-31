74ef1491ca29ab2e419fa3e6c69fa9700aab1525
Camilla Löwy <elmindreda@elmindreda.org>
2018-07-31 14:17:27 +0200

    Woah, upgrades

--- main.c
+++ main.c
@@ -1,18 +1,22 @@
 
 #include <windows.h>
 
-BOOL WINAPI DllMain(HINSTANCE instance, DWORD reason, VOID* reserved)
+static BOOL CALLBACK thread_window_callback(HWND window, LPARAM lParam)
 {
-    return TRUE;
-}
-
-LONG __declspec(dllexport) Blue(void)
-{
-    const HWND window = GetForegroundWindow();
     SetClassLongPtrA(window,
                      GCL_HBRBACKGROUND,
                      (LONG) CreateSolidBrush(RGB(0, 0, 64)));
     RedrawWindow(window, NULL, NULL, RDW_ERASE | RDW_INVALIDATE);
+}
+
+LONG __declspec(dllexport) blue(void)
+{
+    EnumThreadWindows(GetCurrentThreadId(), thread_window_callback, 0);
     return 1;
+}
+
+BOOL WINAPI DllMain(HINSTANCE instance, DWORD reason, VOID* reserved)
+{
+    return TRUE;
 }
 
